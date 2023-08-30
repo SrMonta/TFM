18d8561bd8c7afa2481de15fd80a080029148da9
SrMonta <jorge.alvamonta@gmail.com>
2023-08-30 21:26:01 +0200

    add prompts

---
+++ prompts.py
@@ -0,0 +1,11 @@
+prompts = {
+    "question": {
+        "true/false": "Utilizando el siguiente texto genera 3 preguntas de tipo test de verdadero y falso.",
+        "single": "Utilizando el siguiente texto genera 3 preguntas tipo test con 4 opciones donde solo una de ellas es correcta.",
+        "multiple": "Utilizando el siguiente texto genera 3 preguntas de tipo test. Las preguntas pueden tener máximo 4 opciones. Debe haber al menos 2 opciones correctas y una incorrecta."
+        },
+    "answers": {
+        "true": " Después de cada pregunta indica la respuesta correcta.",
+        "false": ""
+        }
+    }
