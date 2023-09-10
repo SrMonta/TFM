107e02ee3ce528aaea4d6553eadbb942c882d351
SrMonta <jorge.alvamonta@gmail.com>
2023-09-10 18:07:19 +0200

    update prompts

--- prompts.py
+++ prompts.py
@@ -1,11 +1,16 @@
+from config import N_QUESTIONS_JOIN
+
 prompts = {
     "question": {
-        "true/false": "Utilizando el siguiente texto genera 3 preguntas de tipo test de verdadero y falso.",
-        "single": "Utilizando el siguiente texto genera 3 preguntas tipo test con 4 opciones donde solo una de ellas es correcta.",
-        "multiple": "Utilizando el siguiente texto genera 3 preguntas de tipo test. Las preguntas pueden tener máximo 4 opciones. Debe haber al menos 2 opciones correctas y una incorrecta."
+        "true/false": f"Utilizando el siguiente texto genera {N_QUESTIONS_JOIN} preguntas de tipo test de verdadero y falso.",
+        "single": f"Utilizando el siguiente texto genera {N_QUESTIONS_JOIN} preguntas tipo test con 4 opciones donde solo una de ellas es correcta.",
+        "multiple": f"Utilizando el siguiente texto genera {N_QUESTIONS_JOIN} preguntas de tipo test. Las preguntas pueden tener máximo 4 opciones. Debe haber al menos 2 opciones correctas y una incorrecta."
         },
     "answers": {
         "true": " Después de cada pregunta indica la respuesta correcta.",
         "false": ""
+        },
+    "format": {
+            "json": "Devuelve las preguntas como un objeto JSON con 'pregunta', 'opciones' y 'respuesta_correcta'."
         }
     }
